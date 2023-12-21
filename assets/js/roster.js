86116e3af62bd715dc72471baa48269f497f6341
pweetybars <pweetybars@gmail.com>
2023-12-22 03:41:07 +0700

    fixed rosterimage

--- assets/js/roster.js
+++ assets/js/roster.js
@@ -17,22 +17,27 @@ let cardSectionHTML  = ``;
 const player = [
     {
         name: 'Yekindar',
+        image:this.name.toLowerCase(),
         description: 'A smart, aggressive entry player that crack round wide open.'
     },
     {
         name:'Naf',
+        image:this.name.toLowerCase(),
         description: ' CS\'s resident sloth. One of NA\'s greatest. Any Naffers??'
     },
     {
         name: 'Skullz',
+        image:this.name.toLowerCase(),
         description: 'Brazilian Prodigy, Clutch gene. Sharp Shots and Sharper timing'
     },
     {
         name:'CadiaN',
+        image:this.name.toLowerCase(),
         description: 'Top IGL, motivator, innovator, AWPer. Nice in several senses.'
     },
     {
         name:'Twistzz',
+        image:this.name.toLowerCase(),
         description:'The headshot machine TL helped build. One of the best rifler ever, Now back home.'
     }
 ]
